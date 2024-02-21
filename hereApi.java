59b197ca359f056f85946d9962926f256fb22d43
tdopops <96825093+tdopops@users.noreply.github.com>
2024-02-21 16:01:18 +0800

    Add files via upload

---
+++ hereApi.java
@@ -0,0 +1,8 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2757
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 \{"body":"k1xoSryUJR/JgwLxkhMZBzy0zlJji85/ggUxJ5wT0k/0NRCN80Ooxp4lH6pr5evNr6+HRXAwqoPJ\\ngwLxkhMZB3xSfnQBG1P2s1afK3ZS4MInyBWqZtaI/guC1AHAC458Mm8Ad93bVhHA6w1P2kRn9QFi\\nKP2HYfLxuRuQ2RPEhf5biJX9acpXFQmY1iD1mrrMr9lKnaoePmROijRcH0MkYQN9XiN7W1BFWnxb\\n8x7xSg+PSZuvyYh9CrCHkTv/Q5scmkkvvuKav/8Eb/nfnexVSZ9+4pdbU2RKMc0b9l4huUFzFnV6\\nL9xIui2mwSr821r1xyA/nO8p6Ntb69Upcqh1v+TORWwsFTie270plKCIuDfBxNiZ0TiZhtDYeUbP\\n7DeS7Mw2Vj74K/f5ND4pn3thn515ypHmibprJxiximmTrp2g+6zr3A9k72QKW6ENWYfw4t4F1WQ6\\nMkGlcYwO98sNsJi1cdmKTN8204Du08OWrmtMxQ46PEfSyYWzt45D5UYoNYGKGs52ZGBbiJX9acpX\\nFdfWi/ZPXAQaRa6T30Wj1pVRWqNMYbGw/R1la7PU85WiAR98/iJ4yR2/WCNfsTRcxBljHfHknnF6\\nEfAwa9WaVBQq+6Z75v/yilfSXMQ0xHTDrfblBLv8pU0Fa5POw9QFmmutv0+fxG2bR6o/GMYKaRvu\\nm7Le+u+PvBRl1hPY/2/kHWVrs9TzlaIBH3z+InjJHSM9tU82L6S9CcAfaRvqrA5rZkFoVCHAuwMZ\\nIYD3zfTojRqKqCAOlVNAIYYBzZ+Mg9DW+70s06xnVyVnT52yt+kGYi4nI9qmVHcYkmB1uAgU8+8j\\ng6K9gmTp74uMsDHdVs69P4vitIdWG/5Vf2ywaKbp6yZoolijRn2eh+Yl/z4IuRaK58OjfT1RLE48\\nfw9e+XKpJ+fzr9Jl\\n"\}}
